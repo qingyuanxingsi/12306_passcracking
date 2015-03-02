74c2f8b890609e7b2cd780a04e6de5cb17604cd0
Matt-LiYue <liyueam10@gmail.com>
2015-03-01 22:58:21 -0500

    Finished reviews

---
+++ tmp/try.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){//Just try how fast a c program can enumerate the passwords
+int i;
+for (i = 0; i < 1000000000;i++)
+  if (i == 19910228)
+    printf("craked");
+
+return 0;
+}
